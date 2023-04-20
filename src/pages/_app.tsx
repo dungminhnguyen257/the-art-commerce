d0fc27d6f388e2334afc8a4c00c4af6aa2ecceb5
dungminhnguyen257 <nguy7670@mylaurier.ca>
2023-04-19 23:23:05 -0400

    refactor(app.tsx): adding translation to the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import '../styles/global.css';
+import '../styles/globals.css';
 
-import type { AppProps } from 'next/app';
+import { appWithTranslation } from 'next-i18next';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
 
-export default MyApp;
+export default appWithTranslation(MyApp);
